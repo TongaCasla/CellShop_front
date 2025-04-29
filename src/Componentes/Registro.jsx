61d0d38e5b6654092dcf1ebdef348fedc69fbfec
Luciana Itati Fernandez Lezcano <118312382+Luciana2002@users.noreply.github.com>
2025-04-29 04:29:42 -0300

    Update Registro.jsx

--- src/Componentes/Registro.jsx
+++ src/Componentes/Registro.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { useState } from 'react';
 import { Form, Button, Container, Row, Col, FloatingLabel } from 'react-bootstrap';
 
@@ -9,44 +9,43 @@ const Registro = () => {
   };
 
   return (
-    <Container className="m-5 reg">
-      <Row className="justify-content-md-center">
-        <Col md={6}>
-          <h2 className="text-center mb-4">Registro de Usuario</h2>
-          <Form onSubmit={handleSubmit}>
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', minHeight: '65vh' }}>
+      <Container className="m-5 reg">
+        <Row className="justify-content-md-center">
+          <Col md={6}>
+            <h2 className="text-center mb-4">Registro de Usuario</h2>
+            <Form onSubmit={handleSubmit}>
 
-            <FloatingLabel controlId="formNombre" label="Nombre"  className="mx-auto mb-3" style={{ width: '250px' }}>
-              <Form.Control type="text" placeholder="Nombre"  required />
-            </FloatingLabel>
+              <FloatingLabel controlId="formNombre" label="Nombre" className="mx-auto mb-3" style={{ width: '250px' }}>
+                <Form.Control type="text" placeholder="Nombre" required />
+              </FloatingLabel>
 
-            <FloatingLabel controlId="formApellido" label="Apellido"  className="mx-auto mb-3" style={{ width: '250px' }}>
-              <Form.Control type="text" placeholder="Apellido" required />
-            </FloatingLabel>
+              <FloatingLabel controlId="formApellido" label="Apellido" className="mx-auto mb-3" style={{ width: '250px' }}>
+                <Form.Control type="text" placeholder="Apellido" required />
+              </FloatingLabel>
 
-            <FloatingLabel controlId="formEmail" label="Email"  className="mx-auto mb-3" style={{ width: '250px' }}>
-              <Form.Control type="email" placeholder="Email"  required />
-            </FloatingLabel>
+              <FloatingLabel controlId="formEmail" label="Email" className="mx-auto mb-3" style={{ width: '250px' }}>
+                <Form.Control type="email" placeholder="Email" required />
+              </FloatingLabel>
 
-            <FloatingLabel controlId="formPassword" label="Contraseña"  className="mx-auto mb-3" style={{ width: '250px' }}>
-              <Form.Control type="password" placeholder="Contraseña" required />
-            </FloatingLabel>
+              <FloatingLabel controlId="formPassword" label="Contraseña" className="mx-auto mb-3" style={{ width: '250px' }}>
+                <Form.Control type="password" placeholder="Contraseña" required />
+              </FloatingLabel>
 
-            <FloatingLabel controlId="formFechaNacimiento" label="Fecha de Nacimiento"  className="mx-auto mb-3" style={{ width: '250px' }}>
-              <Form.Control type="date" placeholder="Fecha de nacimiento" required />
-            </FloatingLabel>
+              <FloatingLabel controlId="formFechaNacimiento" label="Fecha de Nacimiento" className="mx-auto mb-3" style={{ width: '250px' }}>
+                <Form.Control type="date" placeholder="Fecha de nacimiento" required />
+              </FloatingLabel>
 
-            
+              <Button variant="primary" type="submit" className="mx-auto mb-3 d-block">
+                Registrarse
+              </Button>
 
-            <Button variant="primary" type="submit" className="mx-auto mb-3">
-              Registrarse
-            </Button>
-
-          </Form>
-        </Col>
-      </Row>
-    </Container>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 };
 
 export default Registro;
-
